6240304251ca88778a445a39daa4136e8658e7d2
howu <howuchen@163.com>
2020-04-08 11:20:58 +0800

    创建数据库和表的配置

---
+++ app/src/main/java/com/coolba/weather/db/City.java
@@ -0,0 +1,42 @@
+package com.coolba.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;//城市Id
+    private String cityName;//城市名
+    private int cityCode;//城市代号
+    private int provinceId;//省份Id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolba/weather/db/County.java
@@ -0,0 +1,42 @@
+package com.coolba.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;//县Id
+    private String countyName;//县名
+    private String weatherId;//天气Id
+    private int cityId;//城市Id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
